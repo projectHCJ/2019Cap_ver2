5e147374daafbd3a8cf8ff0e88945cd84ad271b1
s-jis <s-jis@naver.com>
2019-05-28 11:43:49 +0900

    Toolbar 카페 앱에 맞게 수정

--- app/src/main/java/com/example/a2019cap_ver2/MainActivity.java
+++ app/src/main/java/com/example/a2019cap_ver2/MainActivity.java
@@ -58,9 +58,6 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
         return super.onOptionsItemSelected(item);
     }
@@ -71,17 +68,11 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_home) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_bookmark) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_mypage) {
 
         }
 
